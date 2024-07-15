091ff3a9b7da056f32b0407acfa0d1cd2cdbc7cb
nufc-hub <kmchenouf@gmail.com>
2024-07-15 23:09:46 +0900

    Add constructor

---
+++ src/tree.js
@@ -0,0 +1,9 @@
+import Node from './node.js';
+
+class Tree {
+  constructor(array) {
+    this.root = this.buildTree(array); // The return value of buildTree function.
+  }
+
+  buildTree(array) {}
+}
