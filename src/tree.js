2d2dab919eb2bac1172a51adbbf52e6a6fd5d8c0
nufc-hub <kmchenouf@gmail.com>
2024-09-19 15:31:38 +0900

    Add findValue function

--- src/tree.js
+++ src/tree.js
@@ -6,22 +6,149 @@ class Tree {
   }
 
   buildTree(array) {
+    // Recursive function.
     const build = (start, end) => {
+      // Base case. Recursion stops when start becomes greater than end.
       if (start > end) {
         return null;
       }
-      const mid = Math.floor((start + end) / 2);
-      const node = new Node(array[mid]); //Make mid this.root?
-      // Create a tree node with mid as root.
-      // Recursively do the following steps...
+
+      const mid = Math.floor((start + end) / 2); // Get the value at middle index of the array.
+      const node = new Node(array[mid]); // Set the value at the middle index to be the root.
 
       node.left = build(start, mid - 1);
-      node.right = build(mid + 1, array.length);
+      node.right = build(mid + 1, end);
 
       return node;
     };
 
-    return build(0, array.length - 1); // Check meaning of this
+    // Starts the recursive build function.
+    return build(0, array.length - 1);
+  }
+
+  insert(value) {
+    // Recursive function.
+    const insertRec = (node, value) => {
+      // Base case. If the current node = null then there an empty space has been found
+      // where the node can be inserted. In which case a new node is created and inserted.
+      if (node === null) {
+        return new Node(value);
+      }
+
+      // Checks for duplicates. If the value already exists in the tree it is not added.
+      if (value === node.data) {
+        return node;
+      }
+
+      // If the value is less than the current nodes value, it recursively tries to insert into the left
+      // subtree (node.left)
+      if (value < node.data) {
+        node.left = insertRec(node.left, value);
+      } else {
+        // If the value is greater than the the current nodes value, it recursively tries to insert into the right
+        // subtree (node.right)
+        node.right = insertRec(node.right, value);
+      }
+
+      // Returns current node so that the trees structure is maintained as the recursion unwinds.
+      return node;
+    };
+
+    // Starts the recursive insertRec function.
+    this.root = insertRec(this.root, value);
+  }
+
+  delete(value) {
+    const deleteRec = (node, value) => {
+      // Base case: if node is null value is not present
+      // so it returns null.
+      if (node === null) {
+        return node;
+      }
+
+      if (value < node.data) {
+        // If the value to be deleted is less than the current node
+        // it moves to the left subtree.
+        node.left = deleteRec(node.left, value);
+      } else if (value > node.data) {
+        // If the value to be deleted is greater than the current node
+        // it moves to the right subtree.
+        node.right = deleteRec(node.right, value);
+      } else {
+        // Node to be deleted is found.
+
+        // If node has one or no children.
+        // If node has no children, node value is replaced with null.
+        // If node has one child, node value is replaced with child's value.
+        if (node.left === null) {
+          return node.right;
+        } else if (node.right === null) {
+          return node.left;
+        }
+
+        // If node has two children. Replace the node with the smallest value from the right subtree.
+
+        // Get the smallest value in the right subtree.
+        node.data = this.minValue(node.right);
+
+        // Delete the smallest value in the right subtree from its original location.
+        node.right = deleteRec(node.right, node.data);
+      }
+
+      return node;
+    };
+
+    this.root = deleteRec(this.root, value);
+  }
+
+  // Used to find the smallest value.
+  minValue(node) {
+    let minVal = node.data;
+    while (node.left !== null) {
+      minVal = node.left.data;
+      node = node.left;
+    }
+
+    return minVal;
+  }
+
+  // Used to find a value in the tree.
+  findValue(value) {
+    const findValueRec = (node, value) => {
+      // If the value does not exist.
+      if (node === null) {
+        return 'Value does not exist.';
+      }
+
+      //If the value is found.
+      if (node.data === value) {
+        return node;
+      }
+
+      // Search the left subtree.
+      if (value < node.data) {
+        return findValueRec(node.left, value);
+        // Search the right subtree.
+      } else if (value > node.data) {
+        return findValueRec(node.right, value);
+      }
+    };
+
+    // Starts the recursive findValueRec function.
+    return findValueRec(this.root, value);
+  }
+
+  printTree(node = this.root, prefix = '', isLeft = true) {
+    if (node === null) {
+      return;
+    }
+    if (node.right !== null) {
+      this.printTree(node.right, `${prefix}${isLeft ? '│   ' : '    '}`, false);
+    }
+    console.log(`${prefix}${isLeft ? '└── ' : '┌── '}${node.data}`);
+    if (node.left !== null) {
+      this.printTree(node.left, `${prefix}${isLeft ? '    ' : '│   '}`, true);
+    }
   }
 }
 export default Tree;
