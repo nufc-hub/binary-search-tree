d197fb33baf3df2a1d05aa9ed1d41b7fd660d526
nufc-hub <kmchenouf@gmail.com>
2024-10-08 15:38:52 +0900

    Delete node.js from root directory

--- node.js
+++
@@ -1,7 +0,0 @@
-class Node {
-  constructor(data = null, leftNode = null, rightNode = null) {
-    this.data = data; // The value or key stored in the node.
-    this.leftNode = leftNode; // Reference to a left child node.
-    this.rightNode = rightNode; // Reference to a right child node.
-  }
-}
